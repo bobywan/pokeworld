1d974251f5b273073c9ec179c98afa4a943a8422
Boby <simon.banchet@gmail.com>
2026-05-18 13:07:01 +0200

    Build

--- .next/dev/server/chunks/ssr/pokeworld__next-internal_server_app_page_actions_0~2.b.a.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/pokeworld/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=pokeworld__next-internal_server_app_page_actions_0~2.b.a.js.map
